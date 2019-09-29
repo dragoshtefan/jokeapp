6dfbe1ee24cd25429271ba9e44c2cb3a2c128266
dragos.popescu <dragostepop@gmail.com>
2019-09-29 21:24:00 +0300

    new project

---
+++ src/main/java/ro/dpsdevelopment/jokes/jokeapp/JokeappApplication.java
@@ -0,0 +1,15 @@
+package ro.dpsdevelopment.jokes.jokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeappApplication.class, args);
+	}
+
+	
+
+}
